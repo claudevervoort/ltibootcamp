bcdfdb5965aac720c9928be9b93db02af147e2c1
Claude Vervoort <claude.vervoort@gmail.com>
2018-02-10 13:52:10 -0500

    consume items, show links, add gb page

--- server/course/course_manager.py
+++ server/course/course_manager.py
@@ -4,16 +4,29 @@ from time import time
 
 class LineItem(object):
 
-    def __init__(self):
+    def __init__(self, maximumScore, label, resource_id, tag):
+        self.maximumScore = maximumScore
+        self.label = label
         self.results = []
+
+    @classmethod
+    def from_json(cls, li, label=''):
+        label = li.get('label', label)
+        max_score = li['scoreMaximum']
+        resource_id = li.get('resourceId', '')
+        tag = li.get('tag', '')
+        return cls(max_score, label, resource_id, tag)
+
 
 class ResourceLink(object):
 
-    def __init__(self, label, description, lineitem):
+    def __init__(self, label, description, url, params, lineitem=None):
         self.label = label
         self.id = str(uuid.uuid1())
         self.description = description
+        self.url = url
         self.lineitem = lineitem
+        self.params = params
 
 class Course(object):
 
@@ -33,3 +46,15 @@ class Course(object):
         message['http://imsglobal.org/lti/context'] = self.context
         return message
 
+    def addResourceLinks(self, content_items):
+        for item in content_items:
+            label = item.get('title', '')
+            description = item.get('text', '')
+            url = item.get('url', '')
+            custom = item.get('custom', {})
+            rl = ResourceLink(label, description, url, custom)
+            if 'lineItem' in item:
+                rl.lineitem = LineItem.from_json(item['lineItem'])
+            self.links.append(rl)
+    
+            
--- server/lti_platform.py
+++ server/lti_platform.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect
+from flask import Flask, jsonify, request, render_template, redirect, send_from_directory
 from ltiplatform.ltiplatform_manager import LTIPlatform
 from keys import keys_manager
 from random import randrange
@@ -7,6 +7,10 @@ import jwt
 platform = LTIPlatform('http://localhost:5000')
 app = Flask(__name__)
 course_by_tool = {}
+
+@app.route('/assets/<path:path>')
+def send_js(path):
+    return send_from_directory('assets', path)
 
 @app.route("/")
 def hello():
@@ -48,11 +52,13 @@ def content_item_return(context_id):
     encoded_jwt = request.form['jws_token']
     unverified = jwt.decode(encoded_jwt, verify=False)
     tool = platform.get_tool(unverified['iss'])
-    print('tool:' +  tool.client_id)
     deep_linking_res = jwt.decode(encoded_jwt, 
        key=tool.key['key'].publickey().exportKey(), 
        algorithms=['RS256'],
        audience=platform.url)
+    if ('http://imsglobal.org/lti/content_items' in deep_linking_res):
+        content_items = deep_linking_res['http://imsglobal.org/lti/content_items']
+        platform.get_course(context_id).addResourceLinks(content_items)
     return redirect('/course/'+context_id, code=302)
 
 
@@ -67,3 +73,7 @@ def student_launch(tool_id, link_id):
 @app.route("/course/<course_id>")
 def show_course(course_id):
     return render_template('courseoutline.html', course=platform.get_course(course_id))
+
+@app.route("/course/<course_id>/gradebook")
+def show_gradebook(course_id):
+    return render_template('gradebook.html', course=platform.get_course(course_id))
